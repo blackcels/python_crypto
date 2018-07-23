e3975fceb6f996c2b21d8694d5129fa2df265b17
Unknown <tit-max@live.fr>
2018-07-24 01:07:28 +0200

    Encrypt command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys, re
+from partie_cryptage.huffman import HuffmanCode
 
 """ Séparateur """
 SLASH = "/"
@@ -9,6 +10,8 @@ YES = ["Y","y"]
 SERVER = "server"
 CLIENT = "client"
 IS_SERVER = False
+HUFFMAN = False
+RSA = False
 
 """ Liste des commandes """
 CONNECT = "/connect"
@@ -26,43 +29,67 @@ def valid_ip_address(hostIP):
     test = pat.match(hostIP)
     return test
 
+def docs():
+    print("Command Exemple :")
+    print("\t/connect <ip> => connect to server")
+    print("\t/encrypt <crypto_name> <AUTO/MAN> => select crypto program \n\t\t /!\\if is huffman them possibilitie of manuel MAN or Auto AUTO generate KEY\t /!\\")
+    print("\t/msg <message> => send message without encrypt")
+    print("\t/share => send key to mate \n\t\t /!\\ if nothing crypto program choose them choose one \t\t /!\\ \n\t\t /!\\ if nothing key of uncrypt set them display message in raw\t /!\\")
+    print("\t/exit => exit to application")
 
-""" Début du script """
-print("Are you prefer to be a server or client? ")
-prefer = input("[server/client] #>")
-if SERVER == prefer:
-    IS_SERVER = True
-elif CLIENT == prefer:
-    IS_SERVER = False
-
-while True:
-    enter = input("#>")
+def input_processing(enter):
 
     if len(enter) == 0:
-        enter =" "
+        enter = " "
 
-    if  SLASH == enter[0]:
+    if  SLASH == enter[0] and len(enter) > 1 :
         split_list = enter.split(WHITE_SPACE)
         if CONNECT == split_list[0] and not IS_SERVER:
             ip = ""
             if len(split_list) > 1:
                 ip = split_list[1]
-
             if len(ip) == 0:
                 ip = "127.0.0.1"
-                print("IP is ", ip) 
+                # ICI SET IP ADDRESS CLIENT
+                print("IP is ", ip) # A supprimer
             elif len(ip) > 0 and  valid_ip_address(ip):
                 # TODO Set ip of server mate
-                print("IP is ",ip)
+                # ICI SET IP ADDRESS CLIENT
+                print("IP is ",ip) # A supprimer
             else:
                 print("Ip address is'nt valid")
-                continue
+
+        elif IS_SERVER : 
+            print("You are a server, you have not access at this command !!")
+            docs()
+            
         if MESSAGE == split_list[0]:
             # TODO Send message in clear
             print("msg")
         if ENCRYPT == split_list[0]:
-            # TODO Select cryto program
-            print("encrypt")
+            if len(split_list) > 1:
+                if split_list[1] == "HUFF" and not RSA:
+                    # TODO Huffman
+                    HUFFMAN = True
+                    RSA = False
+                    if len(split_list) > 2 and split_list[2] == "MAN": 
+                        print("is huffman MAN")  # A supprimer
+                        # ICI CODE HUFFMAN MANUEL
+                    else:
+                        print("is huffman AUTO") # A supprimer
+                        # ICI CODE HUFMAN AUTO
+                elif split_list[1] == "RSA" and not HUFFMAN:
+                    # TODO RSA
+                    HUFFMAN = False
+                    RSA = True
+                    print("is RSA") # A supprimer
+                    # ICI RSA
+                else:
+                    print("No cryto program")
+            else:
+                HUFFMAN = False
+                RSA = False
+                docs()
         if SEND_KEY == split_list[0]:
             # TODO Send key
             print("sendkey")
@@ -72,11 +99,13 @@ while True:
             if YES[0] == response or YES[1] == response:
                 # TODO Stop connection
                 sys.exit(0)
-            else:
-                continue
     else:
-        print("Command Exemple :")
-        print("\t/connect <ip> => connect to server")
-        print("\t/msg <message> => send message without encrypt")
-        print("\t/send-key => send key to mate \n\t\t /!\\ if nothing crypto program choose them choose one \t\t /!\\ \n\t\t /!\\ if nothing key of uncrypt set them display message in raw\t /!\\")
-        print("\t/exit => exit to application")
+        docs()
+
+""" Début du script """
+print("Are you prefer to be a server or client? ")
+prefer = input("[server/client] #>")
+if SERVER == prefer:
+    IS_SERVER = True
+elif CLIENT == prefer:
+    IS_SERVER = False
